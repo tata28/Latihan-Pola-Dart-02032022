fc2c0ebc3d7f2fb1dcddcb1f696751d850952c2a
tata28 <54984727+tata28@users.noreply.github.com>
2022-03-03 12:24:02 +0700

    no 2 selesai

--- bin/latihandart_02032022.dart
+++ bin/latihandart_02032022.dart
@@ -1,3 +1,52 @@
+import 'dart:io';
+
+void pola1(int n1) {}
+
+void pola2(int n2) {
+  int del = 0, n = 1, m = n2, _m = n2;
+  if (n2 > 9) {
+    _m = 9;
+  }
+  m = _m;
+
+  for (int i = 0; i < n2; i++) {
+    for (int j = 1; j <= n2; j++) {
+      if (n == 10) {
+        n = 1;
+      }
+      if (j > n2 - del) {
+        stdout.write(" ");
+      } else {
+        stdout.write(n);
+      }
+      n++;
+    }
+
+    for (int j = 1; j <= n2; j++) {
+      if (m == 0) {
+        m = 9;
+      }
+      if (j <= del) {
+        stdout.write(" ");
+      } else {
+        stdout.write(m);
+      }
+      m--;
+    }
+    print(" ");
+    n = 1;
+    m = _m;
+    del++;
+  }
+}
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  // stdout.write("n1 : ");
+  //int n1 = int.parse(stdin.readLineSync()!);
+
+  // stdout.write("n2 : ");
+  //int n2 = int.parse(stdin.readLineSync()!);
+
+  //pola1(5);
+  pola2(8);
 }
